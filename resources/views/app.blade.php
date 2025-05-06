9bf1164b20007a5f90c471d2d239fc6b952af08f
randikasptra <saputrarandika857@gmail.com>
2025-05-06 21:09:55 +0700

    Menambahkan component Navbar

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,16 +1,16 @@
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite('resources/css/app.css')
+    <title>SIPANDAKABULAN</title>
 </head>
 
 <body>
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
-    </h1>
+    <x-navbar></x-navbar>
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/app', function () {
     return view('app');
 });
+
