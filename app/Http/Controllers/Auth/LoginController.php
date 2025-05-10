d6abe4292087f31d7f73fbde86c7b267055a9b18
randikasptra <saputrarandika857@gmail.com>
2025-05-10 23:12:57 +0700

    fix/migrate

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -14,6 +14,7 @@ class LoginController extends Controller
     public function login(Request $request)
     {
         $credentials = $request->only('email', 'password');
+        
 
         if (Auth::attempt($credentials)) {
             return redirect()->intended('/dashboard');
