ef18295a0ef5521c7cef2bee2e1240ad6aebfa1a
randikasptra <saputrarandika857@gmail.com>
2025-05-11 22:44:23 +0700

    Menambah klaster

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -57,6 +57,23 @@ class DashboardController extends Controller
                     // ...poin lainnya
                 ],
             ],
+            [
+                'id' => 'Klaster II: Hak Sipil dan Kebebasan',
+                'title' => 'Klaster I: Hak Sipil dan Kebebasan',
+                'nilai_em' => 146.00,
+                'nilai_maksimal' => 164.00,
+                'progres' => 100,
+                'sub_poin' => [
+                    [
+                        'judul' => 'Peraturan Daerah tentang SIPANDAKABULAN',
+                        'terpenuhi' => '[4/4]',
+                        'nilai' => '88.00',
+                        'maksimal' => '96.00',
+                        'link' => '#',
+                    ],
+                    // ...poin lainnya
+                ],
+            ],
             // ...klaster lainnya
         ];
 
