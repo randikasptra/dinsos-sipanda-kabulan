267faf310c1c5c059918ed9a0ed6c8459292a2e2
randikasptra <saputrarandika857@gmail.com>
2025-05-16 09:58:21 +0700

    setUp/auth

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\FormController;
 
 Route::get('/', function () {
-    return view('auth.login');
-})->name('login');
+    return view('welcome');
+});
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-Route::get('/form/{id}', [FormController::class, 'show'])->name('form.show');
-Route::post('/form/{id}', [FormController::class, 'store'])->name('form.store');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-
-
+require __DIR__.'/auth.php';
