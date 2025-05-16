af6b16b153a0fa588a195d7cd110179db0705876
randikasptra <saputrarandika857@gmail.com>
2025-05-16 09:24:14 +0700

    debugg/bug

--- database/migrations/2025_05_12_115600_create_poin_indikators_table.php
+++ database/migrations/2025_05_12_115600_create_poin_indikators_table.php
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('poin_indikators');
+        Schema::dropIfExists('poins');
     }
 };
