c03db8aa5f94cc453fdf95556b560fe175f86604
randikasptra <saputrarandika857@gmail.com>
2025-05-16 09:25:17 +0700

    debugg/next-bug

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,35 +9,29 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        // Ambil semua klaster beserta relasi poin-nya
         $klasters = Klaster::with('poin')->get();
 
-        // Format data untuk ditampilkan di dashboard
         $formatted = $klasters->map(function ($klaster) {
-            $totalMaksimal = $klaster->poin->sum('nilai_maksimal') ?: 1;
-            $totalNilai = $klaster->poin->sum('nilai');
-            $progres = ($totalNilai / $totalMaksimal) * 100;
-
             return [
                 'id' => $klaster->id,
                 'title' => $klaster->nama,
-                'nilai_em' => round($totalNilai, 2),
-                'nilai_maksimal' => round($totalMaksimal, 2),
-                'progres' => round($progres, 2),
+                'nilai_em' => $klaster->poin->sum('nilai'),
+                'nilai_maksimal' => $klaster->poin->sum('nilai_maksimal'),
+                'progres' => 100,
                 'sub_poin' => $klaster->poin->map(function ($poin) {
                     return [
                         'id' => $poin->id,
                         'judul' => $poin->judul,
-                        'nilai' => round($poin->nilai, 2),
-                        'maksimal' => round($poin->nilai_maksimal, 2),
+                        'nilai' => $poin->nilai,
+                        'maksimal' => $poin->nilai_maksimal,
                         'terpenuhi' => "[{$poin->terpenuhi}/{$poin->total}]",
                     ];
                 })->toArray()
             ];
         })->toArray();
 
-        // Kirim ke view
-        return view('pages.dashboard', ['klasters' => $formatted]);
+        dd($formatted); // debug apakah isi klasternya ada
 
+        return view('pages.dashboard', ['klasters' => $formatted]);
     }
 }
