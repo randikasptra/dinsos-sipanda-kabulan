1f3eb12147d3b9579441a071c34a7db8e2675151
randikasptra <saputrarandika857@gmail.com>
2025-05-19 23:17:05 +0700

    rev/dash-pages

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -1,8 +1,7 @@
-{{-- @extends('components.card-klaster') --}}
 @extends('layouts.dashboard')
 
 @section('content')
-    <div x-data="{ openId: null }" class="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+    <div class="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
         @foreach ($klasters as $klaster)
             <x-card-klaster 
                 :id="$klaster['id']"
@@ -10,11 +9,7 @@
                 :nilai-em="$klaster['nilai_em']"
                 :nilai-maksimal="$klaster['nilai_maksimal']"
                 :progres="$klaster['progres']"
-                :sub-poin="$klaster['sub_poin']"
             />
         @endforeach
     </div>
 @endsection
-
-
-
